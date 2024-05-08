8eb4498315a4567e130482524b1b73280e828da9
Artur <euroru98@gmail.com>
2024-05-08 17:24:48 +0300

    Add ProductPageTest

--- src/test/java/com/example/demo/ProductPage.java
+++ src/test/java/com/example/demo/ProductPage.java
@@ -12,8 +12,8 @@ import java.time.Duration;
 // page_url = https://vk.com/club225299895?w=product-225299895_10044406
 public class ProductPage {
 
-    public static WebDriver driver;
-    public static WebDriverWait wait;
+    public WebDriver driver;
+    public WebDriverWait wait;
 
     public ProductPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
